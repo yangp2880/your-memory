98fdad54e2630e20da8345a1ad47a47874d018e9
yangp2880 <2391892880@qq.com>
2025-12-27 00:20:20 +0800

    n

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div>
+	  这是一个组件
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
